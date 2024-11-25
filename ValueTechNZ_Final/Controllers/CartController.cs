0a56a49259b3de67185b78a0726cc5e7b6aa2455
Jun Anthony <junanthony.business@gmail.com>
2024-11-25 23:47:44 +1300

    fix add to cart cookies in layout

---
+++ ValueTechNZ_Final/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ValueTechNZ_Final.Data;
+using ValueTechNZ_Final.Models;
+
+namespace ValueTechNZ_Final.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _data;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly decimal shippingFee;
+
+        public CartController(ApplicationDbContext data, IConfiguration configuration,
+                              UserManager<ApplicationUser> userManager)
+        {
+            _data = data;
+            _userManager = userManager;
+            shippingFee = configuration.GetValue<decimal>("CartSettings:ShippingFee");
+        }
+        public IActionResult Index()
+        {
+            //List<OrderItem> cartItems = 
+            return View();
+        }
+    }
+}
