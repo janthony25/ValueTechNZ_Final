4d860af62990d1ca6ca8c01464e16df49719fbc0
Jun <junanthony.business@gmail.com>
2024-11-26 09:25:32 +1300

    shopping cart now responsive via carthelper

---
+++ ValueTechNZ_Final/Services/CartHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+
+namespace ValueTechNZ_Final.Services
+{
+    public class CartHelper
+    {
+        public static Dictionary<int, int> GetCartDictionary(HttpRequest request, HttpResponse response)
+        {
+            string cookie_value = request.Cookies["shopping_cart"] ?? "";
+
+            try
+            {
+                var cart = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(cookie_value));
+                Console.WriteLine("[CartHelper] cart =" + cookie_value + " -> " + cart);
+                var dictionary = JsonSerializer.Deserialize<Dictionary<int, int>>(cart);
+                
+                if(dictionary != null)
+                {
+                    return dictionary;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            if (cookie_value.Length > 0)
+            {
+                // This cookie is not valid => delete it
+                response.Cookies.Delete("shopping_cart");
+            }
+
+            return new Dictionary<int, int>();
+        }
+
+        public static int GetCartSize(HttpRequest request, HttpResponse response)
+        {
+            int cartSize = 0;
+
+            var cartDictionary = GetCartDictionary(request, response);
+            foreach(var keyValuePair in cartDictionary)
+            {
+                cartSize += keyValuePair.Value;
+            }
+
+            return cartSize;
+        }
+    }
+}
